c60ec490fbdbe1be6eacf170bdbaab0c521dc72e
= <=>
2026-02-19 15:55:59 +0200

    i want to push folder structure

--- obj/Debug/net10.0/PAccountant.AssemblyInfo.cs
+++ obj/Debug/net10.0/PAccountant.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PAccountant")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c7962829eb715603c72512d4f9820829e210fa4b")]
 [assembly: System.Reflection.AssemblyProductAttribute("PAccountant")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PAccountant")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

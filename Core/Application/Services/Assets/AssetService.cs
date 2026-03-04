51e6a31108410967380d67fb4fbcad439fae54e6
= <=>
2026-03-04 13:41:37 +0200

    asset

--- Core/Application/Services/Assets/AssetService.cs
+++ Core/Application/Services/Assets/AssetService.cs
@@ -1,4 +1,3 @@
-using System.Security.Cryptography.X509Certificates;
 using Application.Interfaces;
 using Domain.Entities;
 using Application.DTO;
@@ -6,46 +5,34 @@ using Application.DTO;
 
 namespace Application.Services.Assets
 {
-    
     public class AssetService : IAssetService
     {
         private readonly IAsset _asset;
 
-        //Constructor
         public AssetService(IAsset asset)
         {
             _asset = asset;
         }
-        
-            public List<Asset> GetAllAssets()
-            {
-                List<Asset> assets = _asset.GetAllAssets();
-               return assets;
-            }
 
-            public Asset GetAssetById(int id)
-            {
-               return _asset.GetAssetById(id);
-            }   
+        public List<Asset> GetAllAssets()
+        {
+            List<Asset> assets = _asset.GetAllAssets();
+            return assets;
+        }
 
-            public void CreateAsset(AssetCreateDTO assetDTO)
-            {  
-              _asset.CreateAsset(assetDTO);
-            }
+        public Asset GetAssetById(int id)
+        {
+            return _asset.GetAssetById(id);
+        }
 
-            public void UpdateAsset(int id, AssetUpdateDTO assetUpdateDTO)
-            {
-               _asset.UpdateAsset(id, assetUpdateDTO);
-            }
+        public void CreateAsset(AssetCreateDTO assetDTO)
+        {
+            _asset.CreateAsset(assetDTO);
+        }
 
-    //     List<Domain.Entities.Asset> IAssetService.GetAllAssets()
-    //     {
-    //         throw new NotImplementedException();
-    //     }
-
-    //     Domain.Entities.Asset IAssetService.GetAssetById(int id)
-    //     {
-    //         throw new NotImplementedException();
-    //     }
-    // }
-}}
+        public void UpdateAsset(int id, AssetUpdateDTO assetUpdateDTO)
+        {
+            _asset.UpdateAsset(id, assetUpdateDTO);
+        }
+    }
+}
